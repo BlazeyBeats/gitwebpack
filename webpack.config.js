fe191a3d661330885d42479724d91dbc4795a1a2
Blazey <yoyomiao1999@gmail.com>
2019-04-11 13:29:30 +0800

    install babel bootstrap popper

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,16 @@ module.exports = {
         filename: 'bundle.js',
         publicPath: './dist/'
     },
+    module:{
+        rules:[{
+            test: /\.(js)$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                options:{
+                    presets:['@babel/preset-env']
+                }
+            }
+        }]
+    }
 }
